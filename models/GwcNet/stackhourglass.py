481117835cdf6f11d3e34b15c20feb540d33c386
WeiQin Chuah <33308266+waychin-weiqin@users.noreply.github.com>
2022-10-11 09:21:42 +1100

    Update stackhourglass.py

--- models/GwcNet/stackhourglass.py
+++ models/GwcNet/stackhourglass.py
@@ -223,8 +223,8 @@ class GwcNet(nn.Module):
         
             
     def grad_norm(self, grad):
-        grad = grad.pow(2)
         grad = F.normalize(grad, p=2, dim=1) 
+        grad = grad.pow(2)  # optional
         grad = grad * self.eps
         return grad
         
